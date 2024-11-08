1bc81afc9cf572b91d80ec479945108ba70ce580
Hetari <hetari4py@gmail.com>
2024-11-08 21:12:33 +0300

    feat: separate loading animation from hero section

---
+++ src/components/Loading.vue
@@ -0,0 +1,101 @@
+<template>
+  <div
+    v-if="loading"
+    id="loading"
+    class="fixed inset-0 z-[99999] bg-primary-50 text-primary-950"
+  >
+    <div
+      id="percent"
+      class="mt-[calc(100vh-2ch)] overflow-clip ps-[5vw] text-[35vw] leading-none sm:text-[185px] lg:text-[200px]"
+    >
+      <p id="percent-text" class="">0%</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { tryOnMounted, tryOnUnmounted } from '@vueuse/core';
+
+  import gsap from 'gsap';
+  import { ref } from 'vue';
+
+  const tl = gsap.timeline({
+    defaults: {
+      duration: 3,
+      ease: 'power4.inOut',
+    },
+  });
+  const loading = ref(false);
+
+  const heroImageAnimation = () => {
+    const imgContainer = document.getElementById('hero-img-container');
+    const img = document.getElementById('hero-img');
+    const heroContent = document.getElementById('hero-content');
+    gsap.to([heroContent, imgContainer, img], {
+      delay: 0.1,
+      scale: 1,
+      opacity: 1,
+      y: 0,
+      duration: 1.5,
+      ease: 'expo.inOut',
+      stagger: 0.05,
+      onComplete() {
+        loading.value = false;
+      },
+    });
+  };
+
+  tryOnMounted(() => {
+    // Update the percent text during animation
+    if (loading.value) {
+      loadingAnimation();
+    } else {
+      heroImageAnimation();
+    }
+  });
+
+  const loadingAnimation = () => {
+    // Get DOM elements
+    const loading = document.getElementById('loading') as HTMLDivElement;
+    const percent = document.getElementById('percent') as HTMLDivElement;
+    const percentText = document.getElementById(
+      'percent-text',
+    ) as HTMLDivElement;
+
+    tl.to(percentText, {
+      onUpdate() {
+        percentText.innerHTML = (tl.progress() * 10).toFixed() + '0 %';
+      },
+    });
+
+    tl.to(
+      percent,
+      {
+        marginTop: 10,
+        onComplete: () => {
+          gsap.to(percentText, {
+            delay: 0.5,
+            opacity: 0,
+            duration: 1,
+            ease: 'power4.inOut',
+            onStart: () => {
+              gsap.to(loading, {
+                delay: 1,
+                duration: 1,
+                yPercent: -100,
+                onStart() {
+                  heroImageAnimation();
+                },
+              });
+            },
+          });
+        },
+      },
+      '<',
+    );
+  };
+
+  tryOnUnmounted(() => {
+    tl.kill();
+  });
+</script>
--- src/sections/Hero.vue
+++ src/sections/Hero.vue
@@ -1,16 +1,6 @@
 <template>
   <!-- TODO: Make navbar responsive -->
-  <div
-    id="loading"
-    class="fixed inset-0 z-[99999] bg-primary-50 text-primary-950"
-  >
-    <div
-      id="percent"
-      class="mt-[calc(100vh-2ch)] overflow-clip ps-[5vw] text-[35vw] leading-none sm:text-[185px] lg:text-[200px]"
-    >
-      <p id="percent-text" class="">0%</p>
-    </div>
-  </div>
+  <Loading />
   <div class="h-svh px-[5%] pt-6 lg:px-[10%]">
     <header>
       <Navbar class="h-[10svh]" />
@@ -40,76 +30,7 @@
 </template>
 
 <script setup lang="ts">
-  import { tryOnMounted, tryOnUnmounted } from '@vueuse/core';
   import { heroImg } from '../assets/images';
   import { Navbar } from '../components';
-  import gsap from 'gsap';
-
-  const tl = gsap.timeline({
-    defaults: {
-      duration: 3,
-      ease: 'power4.inOut',
-    },
-  });
-
-  const heroImageAnimation = () => {
-    const imgContainer = document.getElementById('hero-img-container');
-    const img = document.getElementById('hero-img');
-    const heroContent = document.getElementById('hero-content');
-    gsap.to([heroContent, imgContainer, img], {
-      delay: 0.1,
-      scale: 1,
-      opacity: 1,
-      y: 0,
-      duration: 1.5,
-      ease: 'expo.inOut',
-      stagger: 0.05,
-    });
-  };
-
-  tryOnMounted(() => {
-    // Get DOM elements
-    const loading = document.getElementById('loading') as HTMLDivElement;
-    const percent = document.getElementById('percent') as HTMLDivElement;
-    const percentText = document.getElementById(
-      'percent-text',
-    ) as HTMLDivElement;
-
-    // Update the percent text during animation
-    tl.to(percentText, {
-      onUpdate() {
-        percentText.innerHTML = (tl.progress() * 10).toFixed() + '0 %';
-      },
-    });
-
-    tl.to(
-      percent,
-      {
-        marginTop: 10,
-        onComplete: () => {
-          gsap.to(percentText, {
-            delay: 0.5,
-            opacity: 0,
-            duration: 1,
-            ease: 'power4.inOut',
-            onStart: () => {
-              gsap.to(loading, {
-                delay: 1,
-                duration: 1,
-                yPercent: -100,
-                onStart() {
-                  heroImageAnimation();
-                },
-              });
-            },
-          });
-        },
-      },
-      '<',
-    );
-  });
-
-  tryOnUnmounted(() => {
-    tl.kill();
-  });
+  import Loading from '../components/Loading.vue';
 </script>
