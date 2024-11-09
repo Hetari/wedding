fd520bafee73e2ccfeb8c2e4d4a87b60db2ee42d
Hetari <hetari4py@gmail.com>
2024-11-09 23:02:46 +0300

    replace image types

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,13 +1,13 @@
 import heroImg from './hero.webp';
 import storyImg from './story.webp';
-import img1 from './1.png';
-import img2 from './2.png';
-import img3 from './3.png';
+import img1 from './1.webp';
+import img2 from './2.webp';
+import img3 from './3.webp';
 import breakLineSvg from './breakLine1.svg';
-import team1 from './Rectangle 20-1.png';
-import team2 from './Rectangle 20-2.png';
-import team3 from './Rectangle 20-3.png';
-import team4 from './Rectangle 20.png';
+import team1 from './Rectangle 20-1.webp';
+import team2 from './Rectangle 20-2.webp';
+import team3 from './Rectangle 20-3.webp';
+import team4 from './Rectangle 20.webp';
 
 const venueImages = [img2, img1, img3];
 const teamImages = [team1, team2, team3, team4];
