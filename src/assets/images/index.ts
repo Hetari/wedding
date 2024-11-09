50b76d4192698b34f63134d6bfb74b9d333b6964
Hetari <hetari4py@gmail.com>
2024-11-09 22:58:52 +0300

    replace image types

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,5 @@
-import heroImg from './hero.png';
-import storyImg from './story.png';
+import heroImg from './hero.webp';
+import storyImg from './story.webp';
 import img1 from './1.png';
 import img2 from './2.png';
 import img3 from './3.png';
